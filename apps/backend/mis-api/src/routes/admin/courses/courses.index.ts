209a6e497e1395fea1ca9d48038792bbd413e156
kofta999 <mostafaxxx555@gmail.com>
2025-03-06 17:46:35 +0200

    chore: improve admin routes folder structure

---
+++ apps/backend/mis-api/src/routes/admin/courses/courses.index.ts
@@ -0,0 +1,7 @@
+import { createRouter } from "@/lib/create-app";
+// import * as handlers from "./courses.handlers";
+// import * as routes from "./courses.routes";
+
+const router = createRouter();
+
+export default router;
