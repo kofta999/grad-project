cb0fa62cabd782fc2152df0dc251f4bd62258de4
kofta999 <mostafaxxx555@gmail.com>
2025-03-23 14:40:48 +0200

    feat: use server rendering on student application

--- apps/frontend/mis/src/app/dashboard/@student/page.tsx
+++ apps/frontend/mis/src/app/dashboard/@student/page.tsx
@@ -1,5 +1,22 @@
-import { Container } from "@/components/ui/container";
+import ApplicationDetails from "./_components/application-details";
+import { getServerApiClient } from "@/lib/client";
 
 export default async function Page() {
-  return <Container>Welcome to student dashboard</Container>;
+  const apiClient = await getServerApiClient();
+  const response = await apiClient.student.applications.$get();
+
+  if (!response.ok) {
+    // Handle errors
+    const data = await response.json();
+    throw new Error(data.message || "Failed to fetch application details");
+  }
+
+  const data = await response.json();
+  const student = data.application;
+
+  if (!student) {
+    return <div>لا توجد بيانات للطالب.</div>;
+  }
+
+  return <ApplicationDetails student={student} />;
 }
