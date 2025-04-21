a16534903f5b353bb42a7616781d89774cac6075
ZiadElsayed01 <ziadelsayed046@gmail.com>
2025-04-21 15:09:27 +0200

    finish current courses and log courses pages

--- apps/frontend/mis/src/app/dashboard/@admin/courseRegistrations/page.tsx
+++ apps/frontend/mis/src/app/dashboard/@admin/courseRegistrations/page.tsx
@@ -2,11 +2,11 @@
 import { Container } from "@/components/ui/container";
 import React, { useEffect, useState } from "react";
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { SearchBar } from "@/components/ui/search";
 import { Card } from "@/components/ui/card";
@@ -14,12 +14,12 @@ import { CardGrid } from "@/components/ui/card";
 import Image from "next/image";
 import { CardContent } from "@/components/ui/card";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table";
 import { apiClient } from "@/lib/client";
 import toast from "react-hot-toast";
@@ -30,237 +30,234 @@ import { InferResponseType } from "@repo/mis-api";
 import { StudentType, ApplicationType } from "@/lib/types";
 
 type RegisteredCourse = InferResponseType<
-	typeof apiClient.admin.courses.list.$post,
-	200
+  typeof apiClient.admin.courses.list.$post,
+  200
 >[number];
 type CoursesType = RegisteredCourse[];
 
 type SemesterType = "first" | "second" | "third";
 
 export default function currentCourses() {
-	const [student, setStudent] = useState<StudentType | null>(null);
-	const [application, setApplication] = useState<ApplicationType | null>(null);
+  const [student, setStudent] = useState<StudentType | null>(null);
+  const [application, setApplication] = useState<ApplicationType | null>(null);
 
-	const [applicationId, setApplicationId] = useState<number | null>(null);
-	const [academicYear, setAcademicYear] = useState<number | null>(null);
-	const [semester, setSemester] = useState<SemesterType | null>(null);
+  const [applicationId, setApplicationId] = useState<number | null>(null);
+  const [academicYear, setAcademicYear] = useState<number | null>(null);
+  const [semester, setSemester] = useState<SemesterType | null>(null);
 
-	const [courses, setCourses] = useState<CoursesType>([]);
+  const [courses, setCourses] = useState<CoursesType>([]);
 
-	const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-	const getUser = async (id: number | null) => {
-		// Check if id is null or invalid
-		if (id === null || id === 0) {
-			// Reset states if the search input is cleared
-			setStudent(null);
-			setApplication(null);
-			setAcademicYear(null);
-			return;
-		}
+  const getUser = async (id: number | null) => {
+    // Check if id is null or invalid
+    if (id === null || id === 0) {
+      // Reset states if the search input is cleared
+      setStudent(null);
+      setApplication(null);
+      setAcademicYear(null);
+      return;
+    }
 
-		try {
-			const res = await apiClient.admin.applications[":id"].$get({
-				param: { id: id.toString() },
-				query: {},
-			});
-			if (res.status === 200) {
-				const data = await res.json();
-				// Data WILL be there
-				setStudent(data.student!);
-				setApplication(data.application!);
-				setAcademicYear(data.application?.registration?.academicYearId!);
-			} else {
-				toast.error("مستخدم غير موجود");
+    try {
+      const res = await apiClient.admin.applications[":id"].$get({
+        param: { id: id.toString() },
+        query: {},
+      });
+      if (res.status === 200) {
+        const data = await res.json();
+        // Data WILL be there
+        setStudent(data.student!);
+        setApplication(data.application!);
+        setAcademicYear(data.application?.registration?.academicYearId!);
+      } else {
+        toast.error("مستخدم غير موجود");
 
-				setStudent(null);
-				setApplication(null);
-				setAcademicYear(null);
-			}
-		} catch (error) {
-			console.error("Error fetching application:", error);
-			toast.error("فشل العثور علي المستخدم");
-		}
-	};
+        setStudent(null);
+        setApplication(null);
+        setAcademicYear(null);
+      }
+    } catch (error) {
+      console.error("Error fetching application:", error);
+      toast.error("فشل العثور علي المستخدم");
+    }
+  };
 
-	const getCourses = async (
-		applicationId: number,
-		semester: SemesterType,
-		academicYearId: number,
-	) => {
-		if (!applicationId || !semester || !academicYearId) {
-			setCourses([]);
-			return;
-		}
-		try {
-			const res = await apiClient.admin.courses.list.$post({
-				json: {
-					applicationId: applicationId,
-					semester: semester,
-					academicYearId: academicYearId,
-				},
-			});
+  const getCourses = async (
+    applicationId: number,
+    semester: SemesterType,
+    academicYearId: number,
+  ) => {
+    if (!applicationId || !semester || !academicYearId) {
+      setCourses([]);
+      return;
+    }
+    try {
+      const res = await apiClient.admin.courses.list.$post({
+        json: {
+          applicationId: applicationId,
+          semester: semester,
+          academicYearId: academicYearId,
+        },
+      });
 
-			if (res.status === 200) {
-				const data = await res.json();
-				setCourses(data);
-			} else {
-				toast.error("فشل العثور علي المواد");
-			}
-		} catch (error) {
-			console.error("Error fetching courses:", error);
-			toast.error("فشل العثور علي المواد");
-		}
-	};
+      if (res.status === 200) {
+        const data = await res.json();
+        setCourses(data);
+      } else {
+        toast.error("فشل العثور علي المواد");
+      }
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+      toast.error("فشل العثور علي المواد");
+    }
+  };
 
-	const handleSearch = (applicationId: string) => {
-		const id = applicationId ? Number(applicationId) : null;
-		setApplicationId(id);
-		getUser(id);
-	};
+  const handleSearch = (applicationId: string) => {
+    const id = applicationId ? Number(applicationId) : null;
+    setApplicationId(id);
+    getUser(id);
+  };
 
-	useEffect(() => {
-		if (applicationId && semester && academicYear) {
-			getCourses(applicationId, semester, academicYear);
-		} else {
-			setCourses([]);
-		}
-	}, [applicationId, semester, academicYear]);
+  useEffect(() => {
+    if (applicationId && semester && academicYear) {
+      getCourses(applicationId, semester, academicYear);
+    } else {
+      setCourses([]);
+    }
+  }, [applicationId, semester, academicYear]);
 
-	return (
-		<Container>
-			<SearchBar
-				placeholder="ابحث هنا..."
-				onChange={(value) => handleSearch(value as string)}
-			/>
-			<Card>
-				<CardContent>
-					<div className="flex flex-col mb-6 md:flex-row md:gap-6">
-						<div className="image-Container border-2 border-mainColor rounded-lg overflow-hidden flex items-center justify-center">
-							<Image
-								src="/avatar.jpg"
-								alt="placeholder"
-								width={120}
-								height={120}
-							/>
-						</div>
-						<CardGrid className="mt-6 md:mt-0">
-							<p className="text-sm">الاسم / {student?.fullNameAr}</p>
-							<p className="text-sm">تاريخ الميلاد / {student?.dob}</p>
-							<p className="text-sm">الرقم القومي / {student?.idNumber}</p>
-							<p className="text-sm">
-								الدرجة العلمية / {application?.registration?.academicDegree}
-							</p>
-							<p className="text-sm">رقم الهاتف / {student?.phoneNoMain}</p>
-							<p className="text-sm">
-								العام الاكاديمي للتسجيل /{" "}
-								{application?.registration.academicYear}
-							</p>
-						</CardGrid>
-					</div>
-					<Select
-						className="w-full md:w-1/2"
-						onValueChange={(value: "first" | "second" | "third") => {
-							setSemester(value);
-						}}
-					>
-						<SelectTrigger className="w-full md:w-1/2">
-							<SelectValue placeholder="اختر الترم" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectItem value="first">الأول</SelectItem>
-							<SelectItem value="second">الثاني</SelectItem>
-							<SelectItem value="third">الصيفي</SelectItem>
-						</SelectContent>
-					</Select>
-					<Table className="border rounded-md mt-6">
-						<TableHeader className="bg-white">
-							<TableRow>
-								<TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
-									اسم المقرر
-								</TableHead>
-								<TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
-									كود المقرر
-								</TableHead>
-								<TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
-									عدد الساعات
-								</TableHead>
-								<TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
-									التقدير
-								</TableHead>
-							</TableRow>
-						</TableHeader>
-						<TableBody>
-							{courses.length === 0 ? (
-								<TableRow>
-									<TableCell colSpan={4} className="text-center">
-										لا يوجد مواد
-									</TableCell>
-								</TableRow>
-							) : (
-								courses.map((course, index) => (
-									<TableRow
-										key={index}
-										className={`
+  return (
+    <Container>
+      <SearchBar
+        placeholder="ابحث هنا..."
+        onChange={(value) => handleSearch(value as string)}
+        className="mt-10 lg:mt-0"
+      />
+      <Card>
+        <CardContent>
+          <div className="flex flex-col mb-6 md:flex-row md:gap-6">
+            <div className="image-Container border-2 border-mainColor rounded-lg overflow-hidden flex items-center justify-center">
+              <Image
+                src="/avatar.jpg"
+                alt="placeholder"
+                width={120}
+                height={120}
+              />
+            </div>
+            <CardGrid className="mt-6 md:mt-0">
+              <p className="text-sm">الاسم / {student?.fullNameAr}</p>
+              <p className="text-sm">تاريخ الميلاد / {student?.dob}</p>
+              <p className="text-sm">الرقم القومي / {student?.idNumber}</p>
+              <p className="text-sm">
+                الدرجة العلمية / {application?.registration?.academicDegree}
+              </p>
+              <p className="text-sm">رقم الهاتف / {student?.phoneNoMain}</p>
+              <p className="text-sm">
+                العام الاكاديمي للتسجيل /{" "}
+                {application?.registration.academicYear}
+              </p>
+            </CardGrid>
+          </div>
+          <Select
+            className="w-full md:w-1/2"
+            onValueChange={(value: "first" | "second" | "third") => {
+              setSemester(value);
+            }}
+          >
+            <SelectTrigger className="w-full md:w-1/2">
+              <SelectValue placeholder="اختر الترم" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="first">الأول</SelectItem>
+              <SelectItem value="second">الثاني</SelectItem>
+              <SelectItem value="third">الصيفي</SelectItem>
+            </SelectContent>
+          </Select>
+          <Table className="border rounded-md mt-6">
+            <TableHeader className="bg-white">
+              <TableRow>
+                <TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
+                  اسم المقرر
+                </TableHead>
+                <TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
+                  كود المقرر
+                </TableHead>
+                <TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
+                  عدد الساعات
+                </TableHead>
+                <TableHead className="text-right font-medium text-[#96A0B6] w-1/4">
+                  التقدير
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {courses.length === 0 ? (
+                <TableRow>
+                  <TableCell colSpan={4} className="text-center">
+                    لا يوجد مواد
+                  </TableCell>
+                </TableRow>
+              ) : (
+                courses.map((course, index) => (
+                  <TableRow
+                    key={index}
+                    className={`
                       ${index % 2 !== 0 ? "bg-white" : "bg-gray-100"}
                     `}
-									>
-										<TableCell className="text-right">{course.title}</TableCell>
-										<TableCell className="text-right">{course.code}</TableCell>
-										<TableCell className="text-right">
-											{course.totalHours}
-										</TableCell>
-										<TableCell className="text-right">
-											{course.grade ? course.grade : "مسجل على الفصل الحالي"}
-										</TableCell>
-									</TableRow>
-								))
-							)}
-						</TableBody>
-					</Table>
-				</CardContent>
-			</Card>
+                  >
+                    <TableCell className="text-right">{course.title}</TableCell>
+                    <TableCell className="text-right">{course.code}</TableCell>
+                    <TableCell className="text-right">
+                      {course.totalHours}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      {course.grade ? course.grade : "مسجل على الفصل الحالي"}
+                    </TableCell>
+                  </TableRow>
+                ))
+              )}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
 
-			{/* Dialog */}
-			<Container className="p-0">
-				{/* Button to open the dialog */}
-				<Button
-					onClick={() => setIsDialogOpen(true)}
-					disabled={!applicationId || !semester}
-				>
-					صفحة تسجيل المواد
-				</Button>
+      {/* Dialog */}
+      <Container className="p-0">
+        {/* Button to open the dialog */}
+        <Button
+          onClick={() => setIsDialogOpen(true)}
+          disabled={!applicationId || !semester}
+        >
+          صفحة تسجيل المواد
+        </Button>
 
-				{/* Dialog Overlay */}
-				<div
-					className={`overlay bg-black/50 fixed inset-0 flex items-center justify-center z-50 ${
-						isDialogOpen ? "block" : "hidden"
-					}`}
-				>
-					{/* Dialog Content */}
-					<div className="dialog-content bg-white p-3 md:p-6 rounded-lg w-full max-w-2xl h-[90%] overflow-y-scroll m-6 md:m-0">
-						{/* Dialog Header */}
-						<div className="dialog-header flex justify-between items-center mb-4">
-							<button
-								onClick={() => setIsDialogOpen(false)}
-								className="text-gray-500 hover:text-gray-700"
-							>
-								<X className="h-6 w-6" />
-							</button>
-						</div>
+        {/* Dialog Overlay */}
+        <div
+          className={`overlay bg-black/55 fixed inset-0 flex items-center justify-center z-50 ${isDialogOpen ? "block" : "hidden"
+            }`}
+        >
+          {/* Dialog Content */}
+          <div className="dialog-content bg-white p-3 md:p-6 rounded-lg w-full max-w-2xl h-[90%] overflow-y-scroll m-6 md:m-0">
+            {/* Dialog Header */}
+            <div className="dialog-header flex justify-between items-center mb-4">
+              <button onClick={() => setIsDialogOpen(false)} className="absolute top-3 right-5 lg:right-10 text-white">
+                <X className="h-8 w-8" />
+              </button>
+            </div>
 
-						{/* Render the logCourses component */}
-						{applicationId && semester && (
-							<RegisterCourseDialog
-								applicationId={applicationId}
-								semester={semester}
-								userRegisteredCourses={courses}
-								setUserRegisteredCourses={setCourses}
-							/>
-						)}
-					</div>
-				</div>
-			</Container>
-		</Container>
-	);
+            {/* Render the logCourses component */}
+            {applicationId && semester && (
+              <RegisterCourseDialog
+                applicationId={applicationId}
+                semester={semester}
+                userRegisteredCourses={courses}
+                setUserRegisteredCourses={setCourses}
+              />
+            )}
+          </div>
+        </div>
+      </Container>
+    </Container>
+  );
 }
