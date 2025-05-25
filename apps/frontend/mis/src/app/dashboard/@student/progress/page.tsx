a39cee9721eaa6feba104a692a61ca06f85e9080
kofta999 <mostafaxxx555@gmail.com>
2025-05-25 11:38:23 +0300

    chore: format code

--- apps/frontend/mis/src/app/dashboard/@student/progress/page.tsx
+++ apps/frontend/mis/src/app/dashboard/@student/progress/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Container } from "@/components/ui/container";
-import React, { useEffect, useState } from "react";
-import { apiClient } from "@/lib/client";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
