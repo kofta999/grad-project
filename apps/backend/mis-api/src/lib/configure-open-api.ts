0dcbf8eec2e220ec4814ad3113ef99e369862bec
kofta999 <mostafaxxx555@gmail.com>
2025-06-14 22:16:20 +0300

    fix: add type json to openapi config

--- apps/backend/mis-api/src/lib/configure-open-api.ts
+++ apps/backend/mis-api/src/lib/configure-open-api.ts
@@ -1,6 +1,6 @@
 import type { AppOpenAPI } from "./types";
 import { apiReference } from "@scalar/hono-api-reference";
-import packageJSON from "../../package.json";
+import packageJSON from "../../package.json" with { type: "json" };
 
 export default function configureOpenAPI(app: AppOpenAPI) {
   app.doc("/doc", {
