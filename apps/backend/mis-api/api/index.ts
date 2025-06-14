e32f87149dc7fa3b86e26a71cbb319d9d703ebb5
kofta999 <mostafaxxx555@gmail.com>
2025-06-15 01:09:36 +0300

    x

--- apps/backend/mis-api/api/index.ts
+++ apps/backend/mis-api/api/index.ts
@@ -2,8 +2,4 @@ import { handle } from "@hono/node-server/vercel";
 // @ts-ignore
 import app from "../dist/src/app.js";
 
-export const config = {
-  runtime: "nodejs", // NOT 'edge'
-};
-
 export default handle(app);
