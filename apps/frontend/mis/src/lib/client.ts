9b290901559b6f8f5032ebffafd538f430a8e7ea
kofta999 <mostafaxxx555@gmail.com>
2025-06-15 14:41:39 +0300

    fix: x

--- apps/frontend/mis/src/lib/client.ts
+++ apps/frontend/mis/src/lib/client.ts
@@ -1,11 +1,16 @@
 import { hcWithType } from "@repo/mis-api";
 
 const customFetch: typeof fetch = async (input, init) => {
-  const authToken = JSON.parse(localStorage.getItem("loggedInUser") || "{}").accessToken;
   const currentHeaders = new Headers(init?.headers);
-  if (authToken) {
-    currentHeaders.set("Authorization", `Bearer ${authToken}`);
+  let authToken: string | undefined = undefined;
+
+  if (typeof window !== "undefined" && window.localStorage) {
+    authToken = JSON.parse(localStorage.getItem("loggedInUser") || "{}").accessToken;
+    if (authToken) {
+      currentHeaders.set("Authorization", `Bearer ${authToken}`);
+    }
   }
+
   const response = await fetch(input, { ...init, headers: currentHeaders });
   if (response.status === 401 && authToken) {
     console.warn("API client (dynamic) returned 401. Logging out.");
