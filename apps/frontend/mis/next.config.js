b1f3ccf76cb80826be4503de47fd5f739a328848
Mostafa Mahmoud <mostafaxxx555@gmail.com>
2025-07-06 12:24:42 +0300

    Update next.config.js

--- apps/frontend/mis/next.config.js
+++ apps/frontend/mis/next.config.js
@@ -13,6 +13,12 @@ module.exports = {
         hostname: "localhost",
         port: "3002",
       },
+      {
+        protocol: "https", // The image uses HTTPS
+        hostname: "st3.depositphotos.com", // The hostname of the image
+        // You can optionally add a pathname if you want to restrict further, e.g.:
+        // pathname: "/9998432/**",
+      },
     ],
   },
 };
